04189757844848e0b158869bfc0034416ecd3191
Mitchell O'Hair <mitchellohair@gmail.com>
2019-09-17 10:45:50 -0700

    WIP

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+$(document).ready(() => {
+  var tst = new Route();
+  tst.render();
+})
--- assets/js/route.js
+++ assets/js/route.js
@@ -13,27 +13,38 @@ class Route {
   }
 
   render () {
-    $('body').append( $('<div>').attr('id', 'map') );
+    $('.main').empty();
+    // $('.main').append( $('<div>').addClass('map__Container') );
+    // $('.map__Container').append( $('<div>').attr('id', 'map') );
+    $('body').append($('<div>').attr('id', 'map'));
     $('body').after('<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAGcnHGaTjPgQDfwq2MdeCJx60EqC5ud0c&callback=showMap" async defer></script>');
   }
 }
 
 var map;
 function showMap () {
+  var directionsService = new google.maps.DirectionsService();
+  var directionsDisplay = new google.maps.DirectionsRenderer();
+  var chicago = new google.maps.LatLng(41.850033, -87.6500523);
+  var mapOptions = {
+    zoom: 7,
+    center: chicago
+  }
+  map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  directionsDisplay.setMap(map);
+}
 
-  // <style>
-  //   #map {
-  //     height: 100%;
-  // }
-  //     html, body {
-  //     height: 100%;
-  //   margin: 0;
-  //   padding: 0;
-  // }
-  // </style>
-
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8
+function calcRoute() {
+  var start = document.getElementById('start').value;
+  var end = document.getElementById('end').value;
+  var request = {
+    origin: start,
+    destination: end,
+    travelMode: 'DRIVING'
+  };
+  directionsService.route(request, function (result, status) {
+    if (status == 'OK') {
+      directionsDisplay.setDirections(result);
+    }
   });
 }
