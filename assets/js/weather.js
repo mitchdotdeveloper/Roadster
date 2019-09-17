fc06bbdc0bc1977f5a77ecdc4463f0102b510692
aaronmdomingo <aaronmdomingo@gmail.com>
2019-09-17 15:11:22 -0700

    Weather class functionality integrated with trip class

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,8 +1,10 @@
 class Weather {
   constructor(location) {
+    this.renderWeatherData = this.renderWeatherData.bind(this);
+    this.obtainWeatherData = this.obtainWeatherData.bind(this);
     this.location = {
-      lat: location.lat,
-      lng: location.lng
+      lat: location.geometry.location.lat(),
+      lng: location.geometry.location.lng()
     }
     this.dailyWeather = {};
     this.currentWeather = {}
@@ -11,18 +13,21 @@ class Weather {
   processWeatherData() {
     const weatherData = {
       method: 'GET',
-      url: `https://api.darksky.net/forecast/c8c585560e5f64a47e3b64cd50e74e26/${this.location.lat},${this.location.lng}`,
-      success: function(weather) {
-        this.dailyWeather = weather.daily;
-        this.currentWeather = weather.currently;
-      },
+      url: `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/c8c585560e5f64a47e3b64cd50e74e26/${this.location.lat},${this.location.lng}`,
+      success: this.obtainWeatherData,
       error: function(error) {
         console.log(error)
       }
     }
     $.ajax(weatherData);
   }
+  obtainWeatherData(weather) {
+    this.dailyWeather = weather.daily;
+    this.currentWeather = weather.currently;
+    this.renderWeatherData();
+  }
   renderWeatherData() {
-    
+    console.log('Daily Data', this.dailyWeather);
+    console.log('Current Weather', this.currentWeather);
   }
 }
