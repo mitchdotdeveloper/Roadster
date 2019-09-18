2da1bd9df0f0432443bb6b87569553961ef47c81
aaronmdomingo <aaronmdomingo@gmail.com>
2019-09-17 17:46:17 -0700

    Added card styling and defined needed weather variables from API

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,11 +1,13 @@
 class Weather {
   constructor(location) {
+    console.log(location)
     this.renderWeatherData = this.renderWeatherData.bind(this);
     this.obtainWeatherData = this.obtainWeatherData.bind(this);
     this.location = {
       lat: location.geometry.location.lat(),
       lng: location.geometry.location.lng()
     }
+    this.locationName = location.address_components[0].long_name;
     this.dailyWeather = {};
     this.currentWeather = {}
     this.processWeatherData();
@@ -27,6 +29,11 @@ class Weather {
     this.renderWeatherData();
   }
   renderWeatherData() {
+    const weatherSummary = this.dailyWeather.summary;
+    const weatherNow = this.currentWeather.apparentTemperature;
+    const weatherIcon = this.currentWeather.icon;
+    const endLocation = this.locationName;
+
     console.log('Daily Data', this.dailyWeather);
     console.log('Current Weather', this.currentWeather);
   }
